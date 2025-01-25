b98d1adfee61aa2552946c5f71810f31c3ef5390
Edgar Rafael Jr. <202210027@fit.edu.ph>
2025-01-25 19:37:06 +0800

    adding 2 htmls and update on app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return '<h1>e-Xtract</h1>'
+    return render_template(url_for('index.html'))
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template(url_for('about.html'))
 
 if __name__ == '__main__':
     app.run()
